33afdd94864f2ae313dd4f67e111064d8eb7a72d
Tanuj Nagpal <tanujnagpal999@gmail.com>
2019-07-10 23:05:10 +0530

    Recallibrate meta info query

--- app.js
+++ app.js
@@ -2,20 +2,10 @@ const Profile = require("./lib/app.js");
 const express = require("express");
 const app = express();
 
-app.get("/api/:username", (req, res) => {
+app.get("/api/:username", async (req, res) => {
   let profile = new Profile(req.params.username);
-  profile
-    .getMetaData()
-    .then(() => {
-      profile.getPostData().then(() => {
-        console.log(profile);
-        res.json(profile);
-        res.end();
-      });
-    })
-    .catch(err => {
-      throw err;
-    });
+  let data = await profile.getMetaData();
+  res.json(data).end();
 });
 app.get("/api", (req, res) => {
   res.status(400);
@@ -26,4 +16,3 @@ app.get("/api", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("up and running");
 });
-
