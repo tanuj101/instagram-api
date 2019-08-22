36a76606815667579a9de9695fb8a038b1127c04
Rafael Cascalho <cascalhoeng@gmail.com>
2019-08-22 19:07:43 -0300

    Fixed the bug refactoring the app from the lib folder + changed the promises to async / await

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const getData = require('./lib/app.js').getData;
 const getMetaData = require('./lib/app.js').getMetaData;
-exports.getPostData=getData;
-exports.getMetaData=getMetaData;
+exports.getPostData = getData;
+exports.getMetaData = getMetaData;
