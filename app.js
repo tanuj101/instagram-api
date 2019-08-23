7caefac643e54616c410706448e1febaae3b283a
Tanuj Nagpal <tanujnagpal999@gmail.com>
2019-08-23 19:33:09 +0530

    Changed the way data was retrieved

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const Profile = require('./lib/Profile.js');
-const express = require('express');
+const Profile = require("./lib/Profile.js");
+const express = require("express");
 const app = express();
 
-app.get('/api/:username', async (req, res) => {
+app.get("/api/meta/:username", async (req, res) => {
   let NewProfile = new Profile(req.params.username);
   try {
-    let data = await NewProfile.getMetaData();
-    return res.json(data).end();
+    await NewProfile.getMetaData();
+    return res.json(NewProfile).end();
   } catch (error) {
     console.error(error);
-    return res.status(500).send('Internal server error');
+    return res.status(500).send(error.message);
+  }
+});
+app.get("/api/full/:username", async (req, res) => {
+  let NewProfile = new Profile(req.params.username);
+  try {
+    await NewProfile.getMetaData();
+    await NewProfile.getPostData();
+    return res.json(NewProfile).end();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error.message);
   }
 });
 
-app.get('/api', (req, res) => {
-  res.status(400);
-  res.send('Username cannot be empty');
-  res.end();
-});
-
 app.listen(process.env.PORT || 3000, () => {
-  console.log('up and running');
+  console.log("up and running");
 });
