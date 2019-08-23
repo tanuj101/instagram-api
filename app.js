49bb6d17449f68994619931787fec1ceddf53b26
Rafael Cascalho <cascalhoeng@gmail.com>
2019-08-23 10:26:50 -0300

    Fixed the bug refactoring the app from the lib folder + changed the promises to async / await (#3)

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const Profile = require("./lib/app.js");
-const express = require("express");
+const Profile = require('./lib/Profile.js');
+const express = require('express');
 const app = express();
 
-app.get("/api/:username", async (req, res) => {
-  let profile = new Profile(req.params.username);
-  let data = await profile.getMetaData();
-  res.json(data).end();
+app.get('/api/:username', async (req, res) => {
+  let NewProfile = new Profile(req.params.username);
+  try {
+    let data = await NewProfile.getMetaData();
+    return res.json(data).end();
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Internal server error');
+  }
 });
-app.get("/api", (req, res) => {
+
+app.get('/api', (req, res) => {
   res.status(400);
-  res.send("Username cannot be empty");
+  res.send('Username cannot be empty');
   res.end();
 });
-// config buildpacks
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log("up and running");
+  console.log('up and running');
 });
