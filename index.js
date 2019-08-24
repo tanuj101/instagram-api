6e26490cb6c4c1cef1b9633ac7a9e215b08ee14e
Tanuj Nagpal <tanujnagpal999@gmail.com>
2019-08-24 15:00:36 +0530

    Rebasing commits

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-const getData = require('./lib/app.js').getData;
-const getMetaData = require('./lib/app.js').getMetaData;
-exports.getPostData = getData;
-exports.getMetaData = getMetaData;
+module.exports = require("./lib/Profile");
