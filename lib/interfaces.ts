18def4be65139b2b9e5b2fe97a16e7d14d611857
Nishkal <kashyapnishkal@gmail.com>
2019-08-30 23:21:53 +0530

    interface updates

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -9,161 +9,161 @@ export interface PostData {
 
 export interface UserInfo {
     biography: string
-    blocked_by_viewer: false
+    blocked_by_viewer: boolean
     business_category_name: null
     connected_fb_page: null
-    country_block: false
-    external_url: "https://nishkal.in/"
-    external_url_linkshimmed: "https://l.instagram.com/?u=https%3A%2F%2Fnishkal.in%2F&e=ATOeDZfPIFQE1sBeSL_NaRRm2NB9VLScqdiu3co5QcypLOzUqhoGbiuXjpJX4pp2semNRr_DxgrhMgQeTQ5E6yNkr_UdP5gE"
-    followed_by_viewer: false
-    follows_viewer: false
-    full_name: "Nishkal Kashyap"
-    has_blocked_viewer: false
-    has_channel: true
-    has_requested_viewer: false
+    country_block: boolean
+    external_url: string
+    external_url_linkshimmed: string
+    followed_by_viewer: boolean
+    follows_viewer: boolean
+    full_name: string
+    has_blocked_viewer: boolean
+    has_channel: boolean
+    has_requested_viewer: boolean
     highlight_reel_count: 3
-    id: "1286030747"
-    is_business_account: false
-    is_joined_recently: false
-    is_private: false
-    is_verified: false
-    profile_pic_url: "https://instagram.fslv1-1.fna.fbcdn.net/vp/1329f0d500f08d216adc601c9e227b78/5E0CCB05/t51.2885-19/11375208_152696325064090_809584850_a.jpg?_nc_ht=instagram.fslv1-1.fna.fbcdn.net"
-    profile_pic_url_hd: "https://instagram.fslv1-1.fna.fbcdn.net/vp/1329f0d500f08d216adc601c9e227b78/5E0CCB05/t51.2885-19/11375208_152696325064090_809584850_a.jpg?_nc_ht=instagram.fslv1-1.fna.fbcdn.net"
-    requested_by_viewer: false
-    username: "nishkalkashyap"
+    id: string
+    is_business_account: boolean
+    is_joined_recently: boolean
+    is_private: boolean
+    is_verified: boolean
+    profile_pic_url: string
+    profile_pic_url_hd: string
+    requested_by_viewer: boolean
+    username: string
 }
 
 interface ExternalData {
     edge_felix_combined_draft_uploads: {
-        count: 0
+        count: number
         edges: []
-        page_info: { has_next_page: false, end_cursor: null }
+        page_info: { has_next_page: boolean, end_cursor: null }
     }
     edge_felix_combined_post_uploads: {
-        count: 0
+        count: number
         edges: []
-        page_info: { has_next_page: false, end_cursor: null }
+        page_info: { has_next_page: boolean, end_cursor: null }
     }
     edge_felix_drafts: {
-        count: 0
+        count: number
         edges: []
-        page_info: { has_next_page: false, end_cursor: null }
+        page_info: { has_next_page: boolean, end_cursor: null }
     }
     edge_felix_pending_draft_uploads: {
-        count: 0
+        count: number
         edges: []
-        page_info: { has_next_page: false, end_cursor: null }
+        page_info: { has_next_page: boolean, end_cursor: null }
     }
     edge_felix_pending_post_uploads: {
-        count: 0
+        count: number
         edges: []
         page_info: {
             end_cursor: null
-            has_next_page: false
+            has_next_page: boolean
         }
     }
     edge_felix_video_timeline: {
-        count: 0
+        count: number
         edges: []
         page_info: {
             end_cursor: null
-            has_next_page: false
+            has_next_page: boolean
         }
     }
     edge_follow: {
-        count: 146
+        count: number
     }
     edge_followed_by: {
-        count: 157
+        count: number
     }
     edge_media_collections: {
-        count: 0
+        count: number
         edges: []
-        page_info: { has_next_page: false, end_cursor: null }
+        page_info: { has_next_page: boolean, end_cursor: null }
     }
     edge_mutual_followed_by: {
-        count: 0
+        count: number
         edges: []
     }
     edge_owner_to_timeline_media: {
-        count: 18
+        count: number
         edges: {
             node: {
-                comments_disabled: false
-                dimensions: { height: 600, width: 480 }
-                display_url: "https://instagram.fslv1-1.fna.fbcdn.net/vp/7cf91ee4e68c01aee0dff3c9a1f73539/5D6BF49B/t51.2885-15/e35/32443666_2067445483511014_5644502340309352448_n.jpg?_nc_ht=instagram.fslv1-1.fna.fbcdn.net"
-                edge_liked_by: { count: 38 }
-                edge_media_preview_like: { count: 38 }
+                comments_disabled: boolean
+                dimensions: { height: number, width: number }
+                display_url: string
+                edge_liked_by: { count: number }
+                edge_media_preview_like: { count: number }
                 edge_media_to_caption: {
                     edges: Array<{
                         node: {
-                            text: "When you have to ma"
+                            text: string
                         }
                     }>
                 }
-                edge_media_to_comment: { count: 6 }
+                edge_media_to_comment: { count: number }
                 felix_profile_grid_crop: null
                 gating_info: null
-                id: "1786927044651662348"
-                is_video: true
+                id: string
+                is_video: boolean
                 location: null
-                media_preview: "ACEql0uXzIAD1Q7fw6j9KnuneJfMTnHBH1I+b8P61jaVMEkZD0YZ/Ef/AFs1sX1wsEJPdgQB9R/Qf4UAV4o3I8wHDKSFIH3vU4x+H65rWBrOsLkSRAd0wvfnjP59j+lWJZDlVQ8uwH0AyW/QUeQvMs5opuz3b8x/hRVcpNzkLaNmfgZ2jJwanluBIhiPOMBeMYI4PtzznoKbBdG3+4AS3JJ/IAYPbr65qsdrfdzwMtk5ye+MdB9am3U0v0sXbGZIlIZipJz/APX9K0obuJpAzEKFQ4wDyWPOcDqAP1rnxwfYGtzSSGnbI6qCueoI44/AkfShRs+YTlpY0vtkP979D/hRV/NFXcg4MN8u0YwT1rRtQhlcD7hZUHpjcCfzAzWVUynBGOOn8qRW5cvgI5zGg+QYAUdOQOn1rXtrZbfypQc7htOcDqp9PpWAGO08ngV0Nt/x6wf76/8As1U1sZp3v5GruFFPwKKg0sf/2Q=="
-                owner: { id: "1286030747", username: "nishkalkashyap" }
-                shortcode: "BjMcK34ADwM"
-                taken_at_timestamp: 1527239008
+                media_preview: string
+                owner: { id: string, username: string }
+                shortcode: string
+                taken_at_timestamp: number
                 thumbnail_resources: Array<{
-                    config_height: 150
-                    config_width: 150
-                    src: "https://instagram.fs"
+                    config_height: number
+                    config_width: number
+                    src: string
                 }>
-                thumbnail_src: "https://instagram.fslv1-1.fna.fbcdn.net/vp/589e815f06a399fa1360b23ab5b67171/5D6BED75/t51.2885-15/e35/c0.60.480.480a/32443666_2067445483511014_5644502340309352448_n.jpg?_nc_ht=instagram.fslv1-1.fna.fbcdn.net"
+                thumbnail_src: string
                 video_view_count: 336
-                __typename: "GraphVideo"
+                __typename: string
             }
         }[]
         page_info: {
-            end_cursor: "QVFELVJucW5aT3JnMGdSRmMteXlUYkpHbEt1SE9lNmp5SG5sYXhhSHlhNVJCTGpubjZZRmV2OTViMEp6UXBLTDc5VWotZXF2MEVfODdULWNPbDdaaUJHQw=="
-            has_next_page: true
+            end_cursor: string
+            has_next_page: boolean
         }
     }
     edge_saved_media: {
         count: 146
         edges: {
             node: {
-                accessibility_caption: "Image may contain: one or more people, people standing, text and outdoor"
-                comments_disabled: false
-                dimensions: { height: 1350, width: 1080 }
-                display_url: "https://instagram.fslv1-1.fna"
-                edge_liked_by: { count: 18060 }
-                edge_media_preview_like: { count: 18060 }
+                accessibility_caption: string
+                comments_disabled: boolean
+                dimensions: { height: number, width: number }
+                display_url: string
+                edge_liked_by: { count: number }
+                edge_media_preview_like: { count: number }
                 edge_media_to_caption: {
-                    edges: {
+                    edges: Array<{
                         node: {
                             text: string;
                         }
-                    }[]
+                    }>
                 }
                 edge_media_to_comment: { count: 194 }
                 gating_info: null
-                id: "2117430239677569941"
-                is_video: false
+                id: string
+                is_video: boolean
                 location: null
                 media_preview: null
-                owner: { id: "6691281904", username: "millionaire_process" }
-                shortcode: "B1in5WYptuV"
-                taken_at_timestamp: 1566637376
+                owner: { id: string, username: string }
+                shortcode: string
+                taken_at_timestamp: number
                 thumbnail_resources: {
-                    config_height: 150
-                    config_width: 150
-                    src: "https://instagram.f"
+                    config_height: number
+                    config_width: number
+                    src: string
                 }[]
-                thumbnail_src: "https://instagram.fslv1-1.fna.fbcdn.net/vp/d279e948c96df9318f9b1c758fff526f/5DFF3CD2/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/69538105_1049359058600873_4729691954359463872_n.jpg?_nc_ht=instagram.fslv1-1.fna.fbcdn.net"
-                __typename: "GraphSidecar"
+                thumbnail_src: string
+                __typename: string
             }
         }[]
         page_info: {
-            end_cursor: "QVFDckZmdWhoRzBwdmNWOFhqaU5fcUdlbExxWV9rQ2VickJNaFk1OG5WN1liOVJqWkxNYk9qeDZqR1ZKZkg4RDNmR0xfSG9EekZ0OU9VWEtXckFyWVZXWA=="
-            has_next_page: true
+            end_cursor: string
+            has_next_page: boolean
         }
     }
 }
